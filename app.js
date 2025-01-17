e45e36fb2a3176bb41b1ab61e4fd12fe69c84787
jsanthoshkiran <jsanthoshkiran@gmail.com>
2025-01-17 11:35:31 +0530

    test trigger

--- app.js
+++ app.js
@@ -6,5 +6,5 @@ app.use('/calculator', calculatorRoutes);
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+  console.log(`Server is running on this port ${PORT}`);
 });
